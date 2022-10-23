e6d8d61630e43b6b690947745f0de7924ea088b6
akshay_khode <akshay.khode93@gmail.com>
2022-10-23 21:16:32 +0530

    msg

--- JenkinsFirstProject/src/main/java/com/test/application/HelloWorld.java
+++ JenkinsFirstProject/src/main/java/com/test/application/HelloWorld.java
@@ -1,10 +1,15 @@
 package com.test.application;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld=======Started ");
+		SpringApplication.run(HelloWorld.class, args);
 	}
 
 }
